bd0396e8f34acf8d0fbfc06a2f009ed1fae3ff29
Awais Khan <awais.khan@jeeny.me>
2025-09-10 23:53:05 +0500

    update checkbox checked and unchecked color

--- app/src/main/java/com/ak/composehotelui/checkbox/TodoScreen.kt
+++ app/src/main/java/com/ak/composehotelui/checkbox/TodoScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -77,7 +78,12 @@ fun TodoScreen(
                     checked = state.isChecked,
                     onCheckedChange = {
                         onAction(TodoActions.OnTextStrikeThrough(isChecked = it))
-                    }
+                    },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color(0xFF53A954),
+                        uncheckedColor = Color.Gray,
+                        checkmarkColor = Color.White
+                    )
                 )
             }
         }
