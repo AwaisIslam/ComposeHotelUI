2a8e9f2f09546ab10ef8f545bf38d244770716f2
Awais Khan <awais.khan@jeeny.me>
2025-09-16 21:47:35 +0500

    Advanced Layouts, Measurements & Advanced Modifiers: Size Modifiers in depth

--- app/src/main/java/com/ak/composehotelui/advanced_layouts/SizeModifiers.kt
+++ app/src/main/java/com/ak/composehotelui/advanced_layouts/SizeModifiers.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,18 +29,16 @@ fun SizeModifiersDemo(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .height(100.dp)
                 .printConstraints("Before 1. fillMaxWidth")
-                .fillMaxWidth(0.5f)
+                .requiredWidth(300.dp)
+                .background(Color.Green)
+                .wrapContentWidth(
+                    align = Alignment.CenterHorizontally
+                )
                 .printConstraints("After 1. fillMaxWidth")
                 .background(Color.Yellow)
-        )
-        Box(
-            modifier = Modifier
-                .height(100.dp)
-                .printConstraints("Before 2. fillMaxWidth")
-                .fillMaxWidth(0.5f)
-                .printConstraints("After 2. fillMaxWidth")
-                .background(Color.Green)
-        )
+        ){
+            Text("Hello World")
+        }
     }
 }
 
