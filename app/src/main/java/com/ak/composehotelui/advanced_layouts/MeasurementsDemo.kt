7f16e7e64bb07c41623764430032b80c1d2cdd64
Awais Khan <awais.khan@jeeny.me>
2025-09-16 20:33:57 +0500

    Advanced Layouts, Measurements & Advanced Modifiers: How Constraints work

--- app/src/main/java/com/ak/composehotelui/advanced_layouts/MeasurementsDemo.kt
+++ app/src/main/java/com/ak/composehotelui/advanced_layouts/MeasurementsDemo.kt
@@ -8,11 +8,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import com.ak.composehotelui.ui.theme.ComposeHotelUITheme
+import kotlin.math.max
+import kotlin.math.min
+
+private val boundedConstraints = Constraints(
+    minWidth = 50,
+    maxWidth = 100,
+    minHeight = 50,
+    maxHeight = 100
+)
+
+private val unBoundedConstraints = Constraints()
+
+private val exactConstraints = Constraints(
+    minWidth = 200,
+    maxWidth = 200,
+    minHeight = 100,
+    maxHeight = 100
+)
+private val combinedConstraints = Constraints(
+    minWidth = 50,
+    maxWidth = 50,
+    minHeight = 50,
+    maxHeight = Constraints.Infinity
+)
 
 @Composable
 fun MeasurementsDemo(modifier: Modifier = Modifier) {
+
     Column(
         modifier = modifier
             .background(Color.Red)
