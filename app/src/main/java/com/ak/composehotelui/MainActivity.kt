e9aca46859ce04fb61773d0a690a366c82c605fe
Awais Khan <awais.khan@jeeny.me>
2025-09-17 21:45:16 +0500

    Advanced Layouts, Measurements & Advanced Modifiers:SubCompose Custom Layouts

--- app/src/main/java/com/ak/composehotelui/MainActivity.kt
+++ app/src/main/java/com/ak/composehotelui/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ak.composehotelui.advanced_layouts.SubComposePagedRow
 import com.ak.composehotelui.checkbox.TodoScreenRoot
-import com.ak.composehotelui.dragable.DraggableModifiers
 import com.ak.composehotelui.ui.theme.ComposeHotelUITheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +41,33 @@ class MainActivity : ComponentActivity() {
                     //NumberGuessScreenRoot(modifier = Modifier.padding(innerPadding))
                     //TodoScreenRoot(modifier = Modifier.padding(innerPadding))
                     //FocusManagement(modifier = Modifier.padding(innerPadding))
-                    DraggableModifiers(modifier = Modifier.padding(innerPadding))
+                    //DraggableModifiers(modifier = Modifier.padding(innerPadding))
+                    Column(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        var page by remember {
+                            mutableIntStateOf(0)
+                        }
+                        SubComposePagedRow(
+                            page
+                        ) {
+                            (1..1000).forEach { _ ->
+                                Box(
+                                    Modifier
+                                        .width(Random.nextInt(300).dp)
+                                        .height(100.dp)
+                                        .background(Color(Random.nextInt()))
+                                )
+                            }
+                        }
+                        Button(
+                            onClick = {page++}
+                        ) {
+                            Text(
+                                text = "Go to next page"
+                            )
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/ak/composehotelui/advanced_layouts/CustomLayouts.kt
+++ app/src/main/java/com/ak/composehotelui/advanced_layouts/CustomLayouts.kt
@@ -9,12 +9,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.util.fastMaxOfOrNull
 import com.ak.composehotelui.ui.theme.ComposeHotelUITheme
 
+// For Layout
+// Measure Children -> Measure the layout itself -> Place children
+
+// Sub Comopse layout
+// Measure Children -> subcompose-> Measure the layout itself -> Place children
 @Composable
 fun PagedRow(
     page:Int,
@@ -60,14 +66,65 @@ fun PagedRow(
     }
 }
 
+@Composable
+fun SubComposePagedRow(
+    page:Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    SubcomposeLayout(
+        modifier = modifier
+    ){constraints->
+        val pages = mutableListOf<List<Placeable>>()
+        var currentPage = mutableListOf<Placeable>()
+        var currentPageWidth = 0
+
+        val measurables = subcompose("content",content)
+
+
+        var i = 0
+        for (measureable in measurables) {
+            val placeable = measureable.measure(constraints)
+            i++
+            if (currentPageWidth + placeable.width > constraints.maxWidth){
+                if (pages.size == page){
+                    break
+                }
+                pages.add(currentPage)
+                currentPage = mutableListOf()
+                currentPageWidth = 0
+            }
+            currentPage.add(placeable)
+            currentPageWidth += placeable.width
+        }
+
+        println("we measured $i composables")
+
+        if (currentPage.isNotEmpty()){
+            pages.add(currentPage)
+        }
+
+        val pageItems = pages.getOrNull(page)?: emptyList()
+        val maxHeight = pageItems.fastMaxOfOrNull { it.height } ?: 0
+
+        layout(constraints.maxWidth,maxHeight){
+            var xOffset = 0
+            pageItems.fastForEach { placeable->
+                placeable.place(xOffset,0)
+                xOffset += placeable.width
+            }
+        }
+    }
+}
+
 @Preview(
     showBackground = false
 )
 @Composable
 private fun CustomLayoutsPreview() {
     ComposeHotelUITheme {
-        PagedRow(
-            1
+        SubComposePagedRow(
+            0
         ){
             Box(
                 Modifier.width(300.dp)
