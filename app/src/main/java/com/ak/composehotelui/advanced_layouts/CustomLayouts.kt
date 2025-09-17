64f331b1ad642d8a9ba19fcf26627a5802198b1e
Awais Khan <awais.khan@jeeny.me>
2025-09-17 21:18:03 +0500

    Advanced Layouts, Measurements & Advanced Modifiers:Custom Layouts

---
+++ app/src/main/java/com/ak/composehotelui/advanced_layouts/CustomLayouts.kt
@@ -0,0 +1,94 @@
+package com.ak.composehotelui.advanced_layouts
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastMaxOfOrNull
+import com.ak.composehotelui.ui.theme.ComposeHotelUITheme
+
+@Composable
+fun PagedRow(
+    page:Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ){measurable, constraints->
+        val placeable = measurable.map {
+            it.measure(constraints)
+        }
+
+        val pages = mutableListOf<List<Placeable>>()
+        var currentPage = mutableListOf<Placeable>()
+        var currentPageWidth = 0
+
+        placeable.fastForEach { placeable ->
+            if (currentPageWidth + placeable.width > constraints.maxWidth){
+                pages.add(currentPage)
+                currentPage = mutableListOf()
+                currentPageWidth = 0
+            }
+            currentPage.add(placeable)
+            currentPageWidth += placeable.width
+        }
+
+        if (currentPage.isNotEmpty()){
+            pages.add(currentPage)
+        }
+
+        val pageItems = pages.getOrNull(page)?: emptyList()
+        val maxHeight = pageItems.fastMaxOfOrNull { it.height } ?: 0
+
+        layout(constraints.maxWidth,constraints.maxHeight){
+            var xOffset = 0
+            pageItems.fastForEach { placeable->
+                placeable.place(xOffset,0)
+                xOffset += placeable.width
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = false
+)
+@Composable
+private fun CustomLayoutsPreview() {
+    ComposeHotelUITheme {
+        PagedRow(
+            1
+        ){
+            Box(
+                Modifier.width(300.dp)
+                    .height(100.dp)
+                    .background(Color.Red)
+            )
+            Box(
+                Modifier.width(50.dp)
+                    .height(100.dp)
+                    .background(Color.Yellow)
+            )
+            Box(
+                Modifier.width(75.dp)
+                    .height(100.dp)
+                    .background(Color.Green)
+            )
+            Box(
+                Modifier.width(300.dp)
+                    .height(100.dp)
+                    .background(Color.Blue)
+            )
+        }
+    }
+}
